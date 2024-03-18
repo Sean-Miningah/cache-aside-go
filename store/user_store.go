8441b8d4a13fd761ab88f00efd4795f13dc0b4a6
sean-miningah <seanpminigah@gmail.com>
2024-03-18 11:56:07 +0300

    added redis caching for get users

--- store/user_store.go
+++ store/user_store.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"cache-api/models"
 	"cache-api/repo"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 type UserStoreRepo struct {
@@ -67,10 +69,36 @@ func (s *UserStoreRepo) SeedUserStore(jsonDataPath string) error {
 }
 
 func (data *UserStoreRepo) ListUsers() ([]models.User, error) {
+	ctx := context.Background()
+
+	// Check for users in the cache first
+	cachedUsers, err := data.repos.Cache.Get(ctx, "users").Result()
+	if err == nil {
+		// If users are found in the cache, unmarshal and return them
+		var users []models.User
+		if err := json.Unmarshal([]byte(cachedUsers), &users); err != nil {
+			return nil, fmt.Errorf("error unmarshalling cached users: %v", err)
+		}
+		return users, nil
+	}
+
+	// If users aren't in the cache, fetch them from the database
 	var users []models.User
 	result := data.repos.DB.Find(&users)
 	if result.Error != nil {
 		return nil, result.Error
+	}
+
+	// Marshal users to JSON before storing in the cache
+	usersJSON, err := json.Marshal(users)
+	if err != nil {
+		return users, fmt.Errorf("error marshalling users to JSON: %v", err)
+	}
+
+	// Store fetched users in the cache for future requests
+	err = data.repos.Cache.Set(ctx, "users", usersJSON, 24*time.Hour).Err() // Set appropriate expiration time
+	if err != nil {
+		return users, err
 	}
 
 	return users, nil
